f2c9d93d8e22712f336a19efd586a4a20df81c4d
ivanpabz <ipabelona@gmail.com>
2012-09-29 17:37:08 +0800

    Beta 3.1 - Admin Menu Color Change on Selected

--- application/views/common/admin-panel.php
+++ application/views/common/admin-panel.php
@@ -4,14 +4,14 @@
         &nbsp;<img src="<?php print base_url(); ?>images/icon-48-user.png" width="25" align="absmiddle" />
         &nbsp;
         <span>
-        	<a href=""><?php print lang('rentals_label'); ?></a>
-        	<a href=""><?php print lang('reservations_label'); ?></a>
-            <a href="<?php print site_url('motorbikes/list_motorbikes'); ?>"><?php print lang('motorbike_label'); ?></a>
+        	<a <?php print ((currentMenu() == 'rentals') ? 'style="color: orange; font-weight: bold"' : ''); ?> href=""><?php print lang('rentals_label'); ?></a>
+        	<a <?php print ((currentMenu() == 'reservations') ? 'style="color: orange; font-weight: bold"' : ''); ?> href=""><?php print lang('reservations_label'); ?></a>
+            <a <?php print ((currentMenu() == 'motorbikes') ? 'style="color: orange; font-weight: bold"' : ''); ?> href="<?php print site_url('motorbikes/list_motorbikes'); ?>"><?php print lang('motorbike_label'); ?></a>
             <a href=""><?php print lang('reports_label'); ?></a>
             <?php if($this->session->userdata('admin_id') == '1') { ?>
-            <a href="<?php print site_url('account/account_list'); ?>"><?php print lang('account_label'); ?></a>
+            <a <?php print ((currentMenu() == 'account') ? 'style="color: orange; font-weight: bold"' : ''); ?> href="<?php print site_url('account/account_list'); ?>"><?php print lang('account_label'); ?></a>
             <?php } else { ?>
-            <a href="<?php print site_url('account/account_list/edit/'.$this->session->userdata('admin_id')); ?>"><?php print lang('edit_account_label'); ?></a>
+            <a <?php print ((currentMenu() == 'account') ? 'style="color: orange; font-weight: bold"' : ''); ?> href="<?php print site_url('account/account_list/edit/'.$this->session->userdata('admin_id')); ?>"><?php print lang('edit_account_label'); ?></a>
             <?php } ?>
         </span> 
         <a href="<?php print site_url('manage/logout'); ?>" class="button-custom logout-container"><?php print lang('logout_label'); ?></a>
